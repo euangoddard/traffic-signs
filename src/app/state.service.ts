91d2f8aa6206084ee3bc37503b926b82682657ca
Euan Goddard <euan.goddard@gmail.com>
2018-02-26 21:38:09 +0000

    Persisted scores using localforage

--- src/app/state.service.ts
+++ src/app/state.service.ts
@@ -2,30 +2,43 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { distinctUntilChanged, map, pluck, tap } from 'rxjs/operators';
+import * as localforage from 'localforage';
 
 @Injectable()
 export class StateService {
+  private static STORAGE_KEY = 'hitsBySignSubject';
+
   private hitsBySignSubject = new BehaviorSubject<HitsBySign>({});
 
-  constructor() {}
+  constructor() {
+    this.rehydrate();
+  }
 
   selectHits(sign: string): Observable<number> {
     return this.hitsBySignSubject.pipe(
       pluck<HitsBySign, number>(sign),
       map(v => v || 0),
       distinctUntilChanged(),
-      tap(v => console.log(sign, v)),
     );
   }
 
   reset(): void {
     this.hitsBySignSubject.next({});
+    localforage.removeItem(StateService.STORAGE_KEY);
   }
 
   incrementHits(sign: string): void {
     const hitsBySign = this.hitsBySignSubject.value;
     const hitsForSign = hitsBySign[sign] || 0;
     this.hitsBySignSubject.next({ ...hitsBySign, [sign]: hitsForSign + 1 });
+    localforage.setItem(StateService.STORAGE_KEY, this.hitsBySignSubject.value);
+  }
+
+  private async rehydrate() {
+    const hitsBySignSubject = (await localforage.getItem(
+      StateService.STORAGE_KEY,
+    )) as HitsBySign;
+    this.hitsBySignSubject.next(hitsBySignSubject || {});
   }
 }
 
