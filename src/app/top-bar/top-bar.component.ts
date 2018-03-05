725f4d5e3a2531e862791c68be9bf1aa4a76061d
Euan Goddard <euan.goddard@gmail.com>
2018-03-05 21:37:15 +0000

    Added reset button

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
+import { StateService } from '../state.service';
 
 @Component({
   selector: 'top-bar',
   templateUrl: './top-bar.component.html',
 })
 export class TopBarComponent {
-
-  constructor() {
-  }
-
-
+  constructor(public state: StateService) {}
 }
